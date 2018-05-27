563fb5855419109b3e3c603a40413768c50e2c61
wulfalpha <drace.davidson@gmail.com>
2018-05-26 18:09:49 -0600

    The Beginning

--- netThingy.py
+++ netThingy.py
@@ -1,1 +1,26 @@
 
+import sys
+import socket
+import getopt
+import threading
+import subprocess
+import os
+import os.path
+import struct
+
+from netaddr import IPNetwork, IPAddress
+from ctypes import *
+
+#global Variables
+listen = False
+command = False
+dist = os.uname()
+target = ""
+port = 0
+
+#help Text
+def man()
+
+	print("netThingy is a network Diagnostic Tool")
+	print("Commands are Rather simple")
+	print(" -p is to ping a specific ip address or website")
